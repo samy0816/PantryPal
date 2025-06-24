330a212e5ce752e46697380e448d8ffd4098c28d
samy0816 <72800538+samy0816@users.noreply.github.com>
2025-06-24 22:32:38 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // FIXED
 import Navigation from './pages/navigation.jsx';
-import Chefstart from './pages/Chefstart.jsx';
+import Chefstart from './pages/chefstart.jsx';
 import Bmeter from './pages/bmeter2.jsx';
 import Cookbuddy from './pages/Cookbuddy.jsx';
 import Bmeter2 from './pages/bmeter3.jsx';
