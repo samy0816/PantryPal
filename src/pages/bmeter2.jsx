e7bb9be2c107cacd9c42444aa9ffad40d52cb91f
Samarth Joshi <samarthjoshi1608@gmail.com>
2025-06-28 18:51:48 +0200

    new commit

--- src/pages/bmeter2.jsx
+++ src/pages/bmeter2.jsx
@@ -134,7 +134,7 @@ export default function Bmeter() {
       <h3>🧮</h3>
       <div className="textbox">
         <h2>Want to Try It Out?</h2>
-        <Link to='/bmeter2'>Click here to use our food budget calculator and build your personalized spending plan!</Link>
+        <Link to='/bmeter2'>Click here to try BudgetMeter and explore your personalized grocery spending plan through our interactive shopping simulation!</Link>
         <span className="rightcontainer-arrow"></span>
       </div>
     </div>
